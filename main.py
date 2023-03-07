712fb240775dd18810750d671cf9453bd9a8965c
Kacper765 <75902591+Kacper765@users.noreply.github.com>
2023-03-07 17:10:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from flask import Flask, render_template, request
 main = Flask(__name__)
 
 
-@main.route('/')
+@main.route("/")
 def index():
-    return 'Hello, World!'
+    return "Hello World!"
 
-@main.route('/hello', methods=['GET', 'POST'])
+@main.route("/hello", methods=["GET", "POST"])
 def hello():
-    if request.method == 'POST':
-        name = request.form['name']
-        return f'Hello, {name}!'
-    return render_template('index.html')
+    if request.method == "POST":
+        name = request.form["name"]
+        return f"Hello, {name}!"
+    return render_template("hello.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main.run()
