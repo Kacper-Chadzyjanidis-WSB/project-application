7ae947998901b0c98a607bb31674223221bc2db1
Kacper765 <75902591+Kacper765@users.noreply.github.com>
2023-03-07 16:27:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello world!")
+from flask import Flask, render_template, request
+
+main = Flask(__name__)
+
+@main.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@main.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hello, {name}!'
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    main.run()
